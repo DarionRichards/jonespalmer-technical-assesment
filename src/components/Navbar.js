afdee827f2b10792b776e71324a4f76e7ef99a2c
Darion Richards <darionrichards@outlook.com>
2022-07-19 17:03:15 +0100

    Navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,52 @@ import JonesPalmerLogo from "../assets/JP-LOGO.jpg";
 export const Navbar = () => {
 	return (
 		<>
-			<section className="nav-container">
-				<div className="nav-logo">
-					<a href="/">
-						<img
-							className="jones-palmer-logo"
-							src={JonesPalmerLogo}
-							alt="Jones and Palmer logo"
-						/>
-					</a>
-				</div>
-				<div>
+			<section className="navbar-container">
+				<section className="navbar">
+					<div className="navbar-logo">
+						<a href="/">
+							<img
+								className="jones-palmer-logo"
+								src={JonesPalmerLogo}
+								alt="Jones and Palmer logo"
+							/>
+						</a>
+					</div>
 					<nav>
 						<ul className="nav-btn-container">
-							<li>About</li>
-							<li>Services</li>
-							<li>Our Work</li>
-							<li>Thinking</li>
-							<li>Careers</li>
-							<li>Get In Touch</li>
+							<li>
+								<a href="/" title="About">
+									About Us
+								</a>
+							</li>
+							<li>
+								<a href="/" title="Services">
+									Services
+								</a>
+							</li>
+							<li>
+								<a href="/" title="Our Work">
+									Our Work
+								</a>
+							</li>
+							<li>
+								<a href="/" title="Thinking">
+									Thinking
+								</a>
+							</li>
+							<li>
+								<a href="/" title="Careers">
+									Careers
+								</a>
+							</li>
+							<li className="nav-btn-git">
+								<a href="/" title="Get in touch">
+									Get In Touch
+								</a>
+							</li>
 						</ul>
 					</nav>
-				</div>
+				</section>
 			</section>
 		</>
 	);
